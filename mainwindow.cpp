6a7dfab0b9117d4c86c112bd9f4b54c8f4ecf0fe
hongMoFang <21497936@qq.com>
2018-05-02 17:05:09 +0800

    增加调试语句

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+
+    qDebug() << __FILE__ << __LINE__;
+    qDebug() << __FILE__ << __LINE__;
+    qDebug() << __FILE__ << __LINE__;
+    qDebug() << __FILE__ << __LINE__;
+    qDebug() << __FILE__ << __LINE__;
 }
 
 MainWindow::~MainWindow()
